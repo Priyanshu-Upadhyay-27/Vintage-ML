0f5414e9145cef8425c7b5f88d9bc62dbdc088e2
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-06-21 18:11:14 +0530

    Added some adv to it

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,5 @@
-# Linear Regression - OLS method
+# Linear Regression - OLS method: Ordinary Least Square
+# good for 2D and similar dimension, not suitable for large dimenesion
 
 class LinearRegression:
     def __init__(self, m, b):
