c67042b1bb50a9ae21509afa05d63d14507a021d
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-06-24 01:00:29 +0530

    Understood OLS, implementing it separately only

--- linear_regression.py
+++ linear_regression.py
@@ -1,13 +1,11 @@
 # Linear Regression - OLS method: Ordinary Least Square
 # good for 2D and similar dimension, not suitable for large dimenesion
+import numpy as np
 
 class LinearRegression:
-    def __init__(self, m, b):
-        self.m = m
-        self.b = b
+    def __init__(self):
+        self.weights = None
+        self.intercepts_ = None
+        self.coef_ = None
 
 
-    def fit(self, method = "gd"):
-        pass
-    def predict(self):
-        pass
