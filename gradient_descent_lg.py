55afdc994d35aedc1b0b854686a1e5c93351d51c
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-06-25 22:56:01 +0530

    added the mathematical processing

--- gradient_descent_lg.py
+++ gradient_descent_lg.py
@@ -8,3 +8,25 @@ class GradientDescent:
         self.epochs = ep
         self.loss_history = []
 
+    def fit(self, X, y):
+        X = X.flatten()
+        y = y.flatten()
+        N = len(X)
+
+        for epoch in range(self.epochs):
+            y_pred = self.m * X + self.b
+
+            # The Backward Pass (Gradients)
+            # Notice the np.sum() which replaces your big Sigma symbol.
+            # We divide by N to get the Mean Squared Error gradient, keeping it stable.
+            dm = (-2 / N) * np.sum(X * (y - y_pred))
+            db = (-2 / N) * np.sum(y - y_pred)
+
+            # STEP 3: The Parameter Update (Taking a step down the hill)
+            self.m = self.m - (self.learning_rate * dm)
+            self.b = self.b - (self.learning_rate * db)
+
+            # Optional: Calculate and save the current loss to see if it's going down
+            current_loss = np.mean((y - y_pred) ** 2)
+            self.loss_history.append(current_loss)
+
